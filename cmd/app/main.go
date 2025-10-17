f37c3e86009ffdee6a055cd4f75c9f97cd5132d4
Alexander Androsov <alexandr.v.androsov@gmail.com>
2025-10-17 11:59:10 +0300

    add env and fixes

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"url-shortener/internal/config"
+
+	"github.com/joho/godotenv"
+)
+
 const (
 	envLocal = "local"
 	envDev   = "dev"
@@ -7,5 +15,11 @@ const (
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Printf("No .env file found")
+	}
 
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
 }
